fb2bdf985f0fb3bfa289dfe61ee711816e320fc2
mbrizion <mbrizion@github.com>
2021-04-12 21:05:39 +0200

    fix ftps

--- setup.sh
+++ setup.sh
@@ -3,7 +3,6 @@
 #colors
 GREEN='\e[0;32m'
 YELLOW='\033[33m'
-CYAN='\e[0;36m'
 END='\e[0;0m'
 
 echo $YELLOW"Cleaning and starting minikube..."$END
@@ -22,29 +21,35 @@ kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manife
 minikube addons enable metallb
 kubectl apply -f srcs/metallb.yaml
 
-#SECRET
-echo $YELLOW"Creating secret..."$END
-kubectl create secret generic mysql-pass --from-literal=password=password
-
 # MYSQL
-echo $YELLOW"Installing MySql"$END
-docker build -t mysql ./srcs/mysql
-kubectl apply -f srcs/mysql.yaml
-
-# WORDPRESS
-echo $YELLOW"Installing Wordpress"$END
-docker build -t wordpress ./srcs/wordpress
-kubectl apply -f srcs/wordpress.yaml
-
-# PMA
-echo $YELLOW"Installing PhpMyAmdin"$END
-docker build -t phpmyadmin ./srcs/phpmyadmin
-kubectl apply -f srcs/phpmyadmin.yaml
+# echo $YELLOW"Installing MySql"$END
+# docker build -t mysql ./srcs/mysql
+# kubectl apply -f srcs/mysql.yaml
 
 # NGINX
-echo $YELLOW"Installing nginx"$END
-docker build -t nginx-service ./srcs/nginx
-kubectl apply -f srcs/nginx-service.yaml
+# echo $YELLOW"Installing nginx"$END
+# docker build -t nginx ./srcs/nginx
+# kubectl apply -f srcs/nginx.yaml
+
+# WORDPRESS
+# echo $YELLOW"Installing Wordpress"$END
+# docker build -t wordpress ./srcs/wordpress
+# kubectl apply -f srcs/wordpress.yaml
+
+# PMA
+# echo $YELLOW"Installing PhpMyAmdin"$END
+# docker build -t phpmyadmin ./srcs/phpmyadmin
+# kubectl apply -f srcs/phpmyadmin.yaml
+
+# INFLUXDB
+echo $YELLOW"Installing influxdb"$END
+docker build -t influxdb ./srcs/influxdb
+kubectl apply -f srcs/influxdb.yaml
+
+# GRAFANA
+echo $YELLOW"Installing grafana"$END
+docker build -t grafana ./srcs/grafana
+kubectl apply -f srcs/grafana.yaml
 
 # FTPS
 # echo $YELLOW"Installing ftps"$END
@@ -55,5 +60,9 @@ echo $YELLOW"Starting dashboard..."$END
 minikube dashboard &
 
 
-echo  $CYAN"pma	:\nid	: user\npass	: password"$END
+echo  $GREEN"pma	:\nid	: user\npass	: password"$END
+echo  $GREEN"------------------------"$END
+echo  $GREEN"ftps	:\nid	: user\npass	: pass"$END
+echo  $GREEN"------------------------"$END
+echo  $GREEN"grafana	:\nid	: admin\npass	: admin"$END
 # docker build -t ftps ./srcs/ftps && kubectl delete -f srcs/ftps.yaml && kubectl apply -f srcs/ftps.yaml
--- srcs/ftps/start_ftps.sh
+++ srcs/ftps/start_ftps.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+FTP_USERNAME=user;
+FTP_PASSWORD=pass;
 
+echo -e "$FTP_PASSWORD\n$FTP_PASSWORD" | adduser -h /mnt/ftps/$FTP_USERNAME $FTP_USERNAME
 vsftpd /etc/vsftpd/vsftpd.conf
